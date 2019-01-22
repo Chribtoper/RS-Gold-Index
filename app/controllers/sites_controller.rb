ea5131e7d019c94eb3e71c93d3970b7f0959d80a
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2019-01-21 20:23:26 -0500

    first commit

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,35 @@
+class SitesController < ApplicationController
+  before_action :find_site, only: [:update, :show]
+
+  def index
+    @sites = Site.all
+    render json: @sites
+  end
+
+  def create
+    @site = Site.create(site_params)
+  end
+
+  def show
+    render json: @site, status: :accepted
+  end
+
+  def update
+    @site.update(strong_params)
+    if @site.save
+      render json: @site, status: :accepted
+    else
+      render json: { errors: @site.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def site_params
+    params.require(:site).permit(:name, :price)
+  end
+
+  def find_site
+    @site = Site.find(params[:id])
+  end
+end
